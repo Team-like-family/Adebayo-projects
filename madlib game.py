d15e90c3275b2e30adbdb723672d4a7ce4e3e643
Team-like-family <150305378+Team-like-family@users.noreply.github.com>
2023-11-09 01:47:31 +0100

    Add files via upload
    Second project

---
+++ madlib game.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Mad lib Game
+
+# In[42]:
+
+
+adjective = input("Enter adjective: ")
+noun = input("Enter noun: ")
+number = input("Enter number: ")
+material = input("Enter material name: ")
+name = input("Enter name: ")
+color = input("Enter number")
+emotion = input("Enter emotion")
+test = f"A Day at the {noun} Once upon a time, in a {adjective} land far, far away, there lived a {noun}.    This {noun} had always dreamed of going on a grand adventure. One {adjective} morning,     the {noun} decided to embark on a journey to discover the hidden treasures of the {noun}.     The {noun} packed a {adjective} backpack with {number} {noun}s, {number} {adjective} {noun}s,    and a {noun} made of {material}. With a {adjective} smile on their face, the {noun} set off on their {adjective}adventure.    As the {noun} walked through the {adjective} forest, they encountered a {adjective} creature.     It had {number} Body Parts and {number} {color} {noun}s all over its body.     The {noun} couldn't help but feel {emotion} when they saw it. The creature introduced itself as {name} and     offered to be the {noun}'s {noun} on their journey. Together, the {noun} and {name} continued their adventure,     crossing {number} {adjective} bridges and climbing {number} [Noun]s. They even found a {noun} made of {material} and     filled with {noun}. The {noun} and {name} shared a {adjective} meal and talked about their {adjective} dreams."
+
+print(test)
+
+
+# In[ ]:
+
+
+
+
